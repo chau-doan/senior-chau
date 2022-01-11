b5fb68f419c8ad722317f07ff4e8706ae5e28466
chaudoan <chaudoan1221@gmail.com>
2022-01-10 20:41:06 -0800

    initial commmit

---
+++ backend/routers/userRouter.js
@@ -0,0 +1,37 @@
+import express from "express";
+import User from "../models/userModel.js";
+import data from "../data.js";
+import expressAsyncHandler from "express-async-handler";
+import bcrypt from 'bcryptjs';
+
+const userRouter = express.Router();
+
+userRouter.get(
+    '/seed',
+    expressAsyncHandler(async (reg, res) => {
+        await User.remove({});
+        const createdUsers = await User.insertMany(data.users);
+        res.send({createdUsers});
+})
+);
+
+userRouter.post('/signin', expressAsyncHandler(async(req, tres) => {
+        const user = await User.findOne({email: req.body.email});
+        if (user) {
+                if(bcrypt.compareSync(req.body.password, user.password)) {
+                        req.send({
+                                _id: user._id,
+                                name: user.name,
+                                email: user.email,
+                                isAdmin: user.isAdmin,
+                                token: generateToken(user),
+                        });
+                        return;
+                }
+        }
+        res.status(401).send({ measage: 'Invalid email or password'});
+
+})
+);
+
+export default userRouter;
